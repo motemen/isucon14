be3b6236d607186a15e93d2f86ef09e274e0a134
Songmu <y.songmu@gmail.com>
2024-12-08 15:03:13 +0900

    format query

--- webapp/go/internal_handlers.go
+++ webapp/go/internal_handlers.go
@@ -47,14 +47,22 @@ func matching() error {
 	matched := &Chair{}
 	empty := false
 	for i := 0; i < 10; i++ {
-		if err := db.GetContext(ctx, matched, "SELECT * FROM chairs INNER JOIN (SELECT id FROM chairs WHERE is_active = TRUE ORDER BY RAND() LIMIT 1) AS tmp ON chairs.id = tmp.id LIMIT 1"); err != nil {
+		if err := db.GetContext(ctx, matched, `
+		SELECT * FROM chairs INNER JOIN (
+			SELECT id FROM chairs WHERE is_active = TRUE ORDER BY id LIMIT 1) AS tmp
+			ON chairs.id = tmp.id LIMIT 1`); err != nil {
 			if errors.Is(err, sql.ErrNoRows) {
 				return nil
 			}
 			return err
 		}
 
-		if err := db.GetContext(ctx, &empty, "SELECT COUNT(*) = 0 FROM (SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (SELECT id FROM rides WHERE chair_id = ?) GROUP BY ride_id) is_completed WHERE completed = FALSE", matched.ID); err != nil {
+		if err := db.GetContext(ctx, &empty, `
+		SELECT COUNT(*) = 0 FROM (
+			SELECT COUNT(chair_sent_at) = 6 AS completed FROM ride_statuses WHERE ride_id IN (
+			SELECT id FROM rides WHERE chair_id = ?
+		) GROUP BY ride_id) is_completed WHERE completed = FALSE`, matched.ID); err != nil {
+
 			return err
 		}
 		if empty {
